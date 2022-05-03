938dc4c4492fbbc3d483a862057018fd5a362460
Mohit <mayank.mohit@gmail.com>
2022-05-03 13:17:44 +0530

    feat: updates quantize package

--- src/color-thief-node.js
+++ src/color-thief-node.js
@@ -1,5 +1,5 @@
 const getPixels = require('get-pixels');
-const quantize = require('quantize');
+const quantize = require('@colorthief/quantize');
 
 function createPixelArray(imgData, pixelCount, quality) {
     const pixels = imgData;
